bd604571c4e1637425e9194d74f3817a31e39af8
Franco De Leon <franco@carbonodev.com>
2020-07-09 21:22:09 -0500

    change navigation title

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,7 +5,7 @@
   >
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <span class="font-semibold text-xl tracking-tight">
-        Franco
+        franc0rk
       </span>
     </div>
     <div class="block lg:hidden">
