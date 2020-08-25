81f824dbb2dc5c4831995fc9f207f6d76c43546c
Franco De Leon <franco@carbonodev.com>
2020-08-25 05:40:15 -0500

    add details to portfolio

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -6,7 +6,7 @@
       class="w-full md:w-1/3 px-4 pb-6"
     >
       <div
-        class="rounded shadow-lg py-6 text-center cursor-pointer"
+        class="rounded shadow-lg p-6 text-center cursor-pointer"
         :class="layoutBgClasses"
         @click="openProject(project)"
       >
@@ -16,7 +16,19 @@
         <div class="h-48">
           <img class="mx-auto h-full" :src="project.thumbnail" />
         </div>
-        <p class="mt-4" :class="layoutTextClasses">{{ project.description }}</p>
+        <p class="mt-4" :class="layoutTextClasses">
+          {{ project.description.substr(0, 60) + '...' }}
+        </p>
+        <div class="flex justify-center items-center">
+          <div
+            v-for="tag in project.tags"
+            :key="tag"
+            class="px-3 py-0 m-3 rounded-full text-white"
+            :style="{ background: bgTags[tag] }"
+          >
+            <span class="text-sm font-bold">{{ tag }}</span>
+          </div>
+        </div>
       </div>
     </div>
     <project-modal
@@ -34,26 +46,45 @@ export default {
       {
         id: 1,
         name: 'JuegosAdaptativos',
-        description: 'Es un sistema de recomendación de videojuegos',
+        description:
+          'Es un sistema de recomendación de videojuegos Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime ex esse est consequuntur, quas praesentium odit deserunt harum, iure sequi voluptatem soluta, sint inventore, fugit eveniet molestias eligendi dolore repellat.',
         thumbnail: 'project1.jpg',
+        tags: ['Laravel', 'Bootstrap'],
+        url: '',
+        attachments: ['project1.jpg', 'project1.jpg', 'project1.jpg'],
       },
       {
         id: 2,
         name: 'Ecosocial',
-        description: 'Red social para apoyo al medio ambiente',
+        description:
+          'This project consists of a both web and mobile application, which pretends to attract people for using it and worry a little bit about environment in order to promote sustainable development.<br><br>Nowadays most of people use social media and actually we do not use it for the best purposes, so <strong>Ecosocial</strong> is a good reason for dealing with a social network and make this world better.',
         thumbnail: 'project2.jpg',
+        tags: ['Vue', 'Quasar'],
+        url: 'https://github.com/franc0rk/ecosocial',
+        attachments: [
+          'project_attachments/ecosocial1.png',
+          'project_attachments/ecosocial2.png',
+          'project_attachments/ecosocial3.png',
+        ],
       },
       {
         id: 3,
         name: 'Eventicket',
-        description: 'An application to make event reservations',
+        description:
+          'An application to make event reservations Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime ex esse est consequuntur, quas praesentium odit deserunt harum, iure sequi voluptatem soluta, sint inventore, fugit eveniet molestias eligendi dolore repellat.',
         thumbnail: 'project3.jpg',
+        tags: ['Laravel', 'Bootstrap'],
+        url: 'https://github.com/franc0rk/eventicket',
+        attachments: ['project3.jpg', 'project3.jpg', 'project3.jpg'],
       },
       {
         id: 4,
         name: 'Binnacle',
         description: 'A hybrid app for perfoming audits',
         thumbnail: 'project4.jpg',
+        tags: ['Vue', 'Quasar', 'Laravel'],
+        url: 'https://app.binnacle.mx',
+        attachments: ['project4.jpg', 'project4.jpg', 'project4.jpg'],
       },
     ],
   }),
@@ -70,10 +101,17 @@ export default {
         'text-gray-900': this.$root.layoutName === 'default',
       }
     },
+    bgTags() {
+      return {
+        Laravel: '#ec0e00',
+        Bootstrap: '#7952b3',
+        Vue: '#4fc08d',
+        Quasar: '#1976d2',
+      }
+    },
   },
   methods: {
     openProject(projectId) {
-      console.log(projectId)
       this.selectedProject = projectId
     },
   },
