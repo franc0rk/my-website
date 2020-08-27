1978c1098ddd6d1a563a0da87dbdc421fe93b0f9
Franco De Leon <francodeleon97@hotmail.com>
2020-08-26 21:10:05 -0500

    add loading bg

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,33 @@
 <template>
-  <div :class="layoutGlobalBg">
+  <div v-if="!loading" :class="layoutGlobalBg">
     <navigation />
     <Nuxt />
     <layout-footer />
+  </div>
+  <div
+    v-else
+    class="w-full h-screen flex items-center justify-center bg-green-500"
+  >
+    <div class="w-32 text-white">
+      <img
+        class="w-full"
+        src="https://3.bp.blogspot.com/-sGwGbveyXFw/WxpvMxhuroI/AAAAAAAeR0Q/nRFES9t4jD0yQhu9HnzSD4bAe1TPmgZUwCLcBGAs/s1600/AW1190604_00.gif"
+      />
+      <p class="text-center">Loading...</p>
+    </div>
   </div>
 </template>
 <script>
 export default {
   layout: 'default',
+  data: () => ({
+    loading: true,
+  }),
+  mounted() {
+    setTimeout(() => {
+      this.loading = false
+    }, 1500)
+  },
   computed: {
     layoutGlobalBg() {
       return {
