0c2097bf4c62a0e39a8bc5224ae0ac9af538cb1d
Franco De Leon <francodeleon97@hotmail.com>
2020-09-08 19:22:00 -0500

    change app name and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Franco De Leon - Frontend Developer',
       },
     ],
     link: [
