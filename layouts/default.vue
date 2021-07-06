efa8911911cfc589f0ee2c604e4034fc78a3bcbc
Franco De Leon <franco@carbonodev.com>
2021-07-05 22:30:56 -0500

    site updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
-  <div v-if="!loading" :class="layoutGlobalBg">
-    <!-- <navigation /> -->
+  <div :class="layoutGlobalBg">
+    <navigation />
     <Nuxt />
-    <!-- <layout-footer /> -->
-  </div>
-  <div
-    v-else
-    class="w-full h-screen flex items-center justify-center bg-green-500"
-  >
-    <div class="w-32 text-white">
-      <img
-        class="w-full"
-        src="https://3.bp.blogspot.com/-sGwGbveyXFw/WxpvMxhuroI/AAAAAAAeR0Q/nRFES9t4jD0yQhu9HnzSD4bAe1TPmgZUwCLcBGAs/s1600/AW1190604_00.gif"
-      />
-      <p class="text-center">Loading...</p>
-    </div>
+    <layout-footer />
   </div>
 </template>
 <script>
